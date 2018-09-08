03ef1b11b2b2f35ba23e050f8e52d0dd7d30b121
Murali <murali.24.4.91@gmail.com>
2018-09-08 17:25:23 -0500

    added comments to code

--- ICP_3/GitHubUserFinder/script.js
+++ ICP_3/GitHubUserFinder/script.js
@@ -1,38 +1,65 @@
+/*
+* This method will make a get call to the Github API and the call appropriate methods.
+* If user found the we are calling showUser method and passing the response.
+* else calling noSuchUser method.
+*/
 function getGithubInfo(user) {
     //1. Create an instance of XMLHttpRequest class and send a GET request using it. The function should finally return the object(it now contains the response!)
-
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(resp) {
+        //if the response is successful show the user's details
+        if(this.readyState == 4 ) {
+            if (this.status == 200) {
+                showUser(JSON.parse(this.responseText));
+                //else display suitable message
+            } else {
+                noSuchUser(username);
+            }
+        }
+    };
+    xhttp.open("GET", "https://api.github.com/users/"+user, true);
+    xhttp.send();
+    return xhttp.responseText;
 }
 
+/*
+* This method will display the Name, image , Github profile and repository links
+*/
 function showUser(user) {
-
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
-
+    $("#profile").find("h2").text(user.name);
+    $(".avatar").prepend('<img src="'+user.avatar_url+'" width="100px" height="100px"/>')
+    $(".information").prepend('<a href="'+user.html_url+'">Github Url</a></br><a href="'+user.repos_url+'">Repo Url</a>');
 }
 
+/*
+* This method will call when user not found. It will display message No such profile username.
+*/
 function noSuchUser(username) {
-    //3. set the elements such that a suitable message is displayed
-
-
+    $("#profile").find("h2").text("No such profile" + username);
 }
 
-
+/*
+* This method will call when the document is ready.
+*/
 $(document).ready(function(){
+    /*
+    * This method will call when user type in the input.
+    * First we are clearing the display content then if user press enter, it will go to if statement.
+    * In If statement we are taking value from input and then storing in username then we are passing username to getGithubInfo(username) method.
+    */
     $(document).on('keypress', '#username', function(e){
         //check if the enter(i.e return) key is pressed
+        $("#profile").find("h2").text("");
+        $(".avatar").html('')
+        $(".information").html('');
         if (e.which == 13) {
             //get what the user enters
             username = $(this).val();
             //reset the text typed in the input
             $(this).val("");
             //get the user's information and store the respsonse
-            response = getGithubInfo(username);
-            //if the response is successful show the user's details
-            if (response.status == 200) {
-                showUser(JSON.parse(response.responseText));
-                //else display suitable message
-            } else {
-                noSuchUser(username);
-            }
+            getGithubInfo(username);
         }
     })
 });
--- ICP_3/PickMyFavoriteColor/script.js
+++ ICP_3/PickMyFavoriteColor/script.js
@@ -1,32 +1,79 @@
 // an array of colors and assign it to a variable colors
-var colors = [ "22ac5e", "d68236", "71b5c2", "af2655", "b34de7", "e6bd01", "104393", "ca4d94", "4a772d", "c180a7", "958112", "8d2f8d" ]
+var colors = ["22ac5e", "d68236", "71b5c2", "af2655", "b34de7", "e6bd01", "104393", "ca4d94", "4a772d", "c180a7", "958112", "8d2f8d"]
+
 // sets the preview color to the one entered in the input and display its color code using setPreviewColor function
 function setPreviewColor(color) {
     $('.preview').css('background-color', color);
     $('.color-code').text($('.preview').css('background-color'));
 }
-//adds color boxes to the favorite colors
+
+/*
+ * addBox method will validate the color and then add color to favorite list
+ * First I'm checking weather the user enter hexa decimal or not. At the same time I'm checking whether the input is empty or not.
+ * if match then I'm adding to favorite list.
+ * else I'm check weather the user selected form displayed colors. if yes then I'm adding to favorite.
+*/
 function addBox(color) {
-    $('#colors').prepend("<div class='item' style='background-color: " + color + ";'><div>");
+    let isOk = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color);
+    if (color != "" && isOk) {
+        $('#colors').prepend("<div class='item' style='background-color: " + color + ";'><div>");
+    } else {
+        if (/rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/.test($('.preview').css('background-color')))
+            $('#colors').prepend("<div class='item' style='background-color: " + $('.preview').css('background-color') + "'><div>");
+    }
+    $('#color').val("");
+    $(".item").bind("click");
 }
 
+/*
+* This method will call when document is ready.
+*/
+$(document).ready(function () {
+    // count is used to iterate the colors array.
+    var count = 0;
+    //Adding predefined colors to table
+    for (var i = 0; i < 3; i++) {
+        var tr = "<tr>";
+        for (var j = 0; j < 4; j++) {
+            tr = tr + "<td style='background-color:" + colors[count] + ";width:100px;height:100px'></td>";
+            count++;
+        }
+        tr = tr + "</tr>"
+        $("#tblcolors").append(tr);
+    }
 
-$(document).ready(function(){
-
-    //1.As the page loads add each color in the colors array to the div '#colors'
-
-
-//set the preview color to one of the colors in the colors array randomly
-    setPreviewColor(colors[Math.floor(Math.random()*colors.length)]);
+    //set the preview color to one of the colors in the colors array randomly
+    setPreviewColor(colors[Math.floor(Math.random() * colors.length)]);
     // an event handler for the key up event i.e. when the user types the color in the input and releases the key on the keyboard
-//The event should set the preview color to the color typed in the input
-    $(document).on('keydown keyup keypress', '#color', function(){
+    //The event should set the preview color to the color typed in the input
+    $(document).on('keydown keyup keypress', '#color', function () {
         color = $(this).val();
         setPreviewColor(color);
     })
-//2.Write an event handler to handle the click the event on the add to favorite button so that the color gets added to the list of favorite colors,
-// the content of the input gets cleared and the focus gets back on the input
 
-//3.Write events handlers such that whenever any item in the favorite colors is clicked or hovered, the color gets displayed in the preview div
+    /*
+    * This method will call when user click on particular td in table of predefined colors.
+    * This will take the value of the hovered td and then fetching the background color and set the color to preview.
+    */
+    $("td").on("click", function () {
+        var color = $(this).css("background-color");
+        setPreviewColor(color);
+    });
 
+    /*
+    * This method will call when user click on add to favorite color.
+    * Taking the input and sending to addBox
+    */
+    $("#add-to-favorite").on("click", function () {
+        addBox($('#color').val())
+    })
+
+    /*
+    * This method will call when user hover the mouse on favorite color.
+    * This will take the value of the hovered div and then fetching the background color and set the color to preview.
+    */
+    $(document).on("mouseover", "#colors .item", function () {
+        let color = $(this).css("background-color");
+        setPreviewColor(color);
+    });
 });
